44a95179a1a158cc816354e19ea77326626a7a6d
S H <zainul2002@gmail.com>
2025-10-27 22:02:18 -0400

    Update Next.js theme to dark gray background for BullGod

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { Footer } from "@/components/footer"
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#2E2DF2] overflow-x-hidden">
+    <main className="min-h-screen bg-[#151515] overflow-x-hidden">
       <Header />
       <Ticker />
       <Hero />
